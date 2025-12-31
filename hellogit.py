5c9ec9dfc50ebd5e6a5cf1025fdc1bd89a68d39f
andrea <andreaepiayuepiayu.com>
2025-12-31 14:51:50 -0500

    Crea script de saludo interactivo en feature-hello

--- hellogit.py
+++ hellogit.py
@@ -1,2 +1,4 @@
-print(" New Hello, Git!")
-print("This is a test commit.")
+print("Hello from feature-hello branch")
+
+name = input("What is your name? ")
+print(f"Welcome to Git workflow, {name}!")
